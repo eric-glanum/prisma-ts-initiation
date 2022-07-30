34d60be60cc37a8a6d9031669fadbd367a271390
eric-glanum <eric@glanum.com>
2022-07-30 21:04:48 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
+
+async function main () {
+    const post = await prisma.post.update({
+        where: { id: 1 },
+        data: { published: true },
+      })
+      console.log(post)
+
+      
+    
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true,
+            profile: true
+        },
+    })
+    console.dir(allUsers, { depth: null })
+}
+
+main()
+    .then(async() => {
+        await prisma.$disconnect()
+    })
+    .catch(async(e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    });
